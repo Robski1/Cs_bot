9614c45560a8cb01eb571934ade7cbc984b27267
Robski <robski0000001@gmail.com>
2020-01-11 16:17:10 +0000

    added .help status

--- CS_Bot.py
+++ CS_Bot.py
@@ -68,6 +68,7 @@ async def errorMsg(ctx):
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(".help"))
     print("Bot is online ")
 
 #### SPEC COMMAND ####
