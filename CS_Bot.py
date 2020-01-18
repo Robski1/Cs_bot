cbc590a35c1c9ea09a56a9c3dd9ced46b7ca7940
BeedPro <59779737+BeedPro@users.noreply.github.com>
2020-01-18 09:51:43 +0000

    Update CS_Bot.py

--- CS_Bot.py
+++ CS_Bot.py
@@ -269,11 +269,9 @@ async def timeLeft():
         
         return (days, hours, minutes, seconds)
 
-   msgSent = False
-
    while True:
         
-        hourArr = ['16','17','18']
+        hourArr = ['16','17']
         minArr = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59']
 
         randHour = random.randint(0,len(hourArr)-1)
@@ -305,7 +303,7 @@ async def timeLeft():
         setTime = setTime[11:]
         setTime = setTime[:5]
 
-        if setTime == '18:30':
+        if setTime == '15:30':
             msgSent = False
                          
 bot.loop.create_task(timeLeft())
