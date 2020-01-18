cf1dd394ef911b220d3e662b9a6ee88b8c041471
BeedPro <59779737+BeedPro@users.noreply.github.com>
2020-01-18 14:18:44 +0000

    Update CS_Bot.py

--- CS_Bot.py
+++ CS_Bot.py
@@ -252,12 +252,7 @@ async def help(ctx, *args):
 #### TimeLeft till first CS exam   ####
 
 async def timeLeft():
-    hourArr = ['16','17']
-    minArr = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59']
     
-    randHour = random.randint(0,len(hourArr)-1)
-    randMin = random.randint(0,len(minArr)-1)
-    randTime = hourArr[randHour]+ ':' + minArr[randMin]
     
     await asyncio.sleep(5)
     channel = bot.get_channel(665690248646492191)
@@ -272,12 +267,17 @@ async def timeLeft():
         hours, minutes = divmod(minutes, 60)
         days, hours = divmod(hours, 24)
         return (days, hours, minutes, seconds)
-    print(randTime)
+        
     msgSent = False
     while True:
+        hourArr = ['16','17']
+        minArr = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59']
+    
+        randHour = random.randint(0,len(hourArr)-1)
+        randMin = random.randint(0,len(minArr)-1)
+        randTime = hourArr[randHour]+ ':' + minArr[randMin]
         
-        
-        if msgSent == False:
+        while not msgSent:
             leaving_date = datetime.strptime('11-05-2020 01:00:00', '%d-%m-%Y %H:%M:%S')
             now =  datetime.now()
             dateFormat = await daysHoursMinutesSecondsFromSeconds(await dateDiffInSeconds(now, leaving_date))
@@ -293,6 +293,9 @@ async def timeLeft():
                 )
                 await channel.send(embed=embed)
                 msgSent = True
+            
+            await asyncio.sleep(10)
+
 
         now = datetime.now()   
         setTime = str(now)
